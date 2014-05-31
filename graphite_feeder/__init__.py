69affff2e44add82c625f0f16f7b33db1f36581c
Nikita Marchant <nikita.marchant@gmail.com>
2014-05-31 23:54:29 +0200

    Use relative import

--- graphite_feeder/__init__.py
+++ graphite_feeder/__init__.py
@@ -2,6 +2,6 @@
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-from feeder import GraphiteFeeder
+from .feeder import GraphiteFeeder
 
 __all__ = ["GraphiteFeeder"]
